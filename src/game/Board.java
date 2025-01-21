7390ef029f086b9b1af4ec641ffaa3253de0eb78
Idris OUJDID <idris.oujdid@edu.esiee-it.fr>
2025-01-21 16:34:27 +0100

    add a validateMovements

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,10 +1,13 @@
 package game;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Board {
 
     private static String[][] board = new String[10][11];
 
-    // Fonction pour initialiser le plateau
+    // Function to initialize the board
     public static void initializeBoard() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 11; j++) {
@@ -13,14 +16,25 @@ public class Board {
         }
     }
 
-    // Fonction pour afficher le plateau
+    // Function to display the board
     public static void displayBoard() {
-        System.out.println("Plateau de jeu : ");
+        System.out.println("Game board: ");
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 11; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
+        }
+
+    }
+
+    // Function to save the score in a file
+    public static void saveScore(String playerName, int score) {
+        try (FileWriter writer = new FileWriter("scores.txt", true)) {
+            writer.write(playerName + ": " + score + "\n");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the score.");
+            e.printStackTrace();
         }
     }
 }
---
+++ src/game/validateMovement.java
@@ -0,0 +1,20 @@
+package game;
+
+public class validateMovement{
+    public static void main(String[] args) {
+        int[][] movement = {
+                { 0, 1 }, // right
+                { 1, 0 }, // bottom
+                { 0, -1 }, // left
+                { -1, 0 } // top
+        };
+        System.out.println(movement);
+    }
+
+    public static boolean isValidMove(int x, int y) {
+        if (isValidMove(x, y)) {
+
+        }
+        return true; // Placeholder
+    }
+}
